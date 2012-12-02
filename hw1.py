e148b963e0d31d08f9e8166db5484dfe72654fe3
Artem <myuutsu@gmail.com>
2012-12-02 17:31:45 +0400

    Added actual .py file

---
+++ hw1.py
@@ -0,0 +1,37 @@
+# Mechanical MOOC / MIT 6-189-a-gentle-introduction-to-programming-using-python / hw1.py
+
+##### Template for Homework 1, exercises 1.2-1.5 ######
+
+##print "********** Exercise 1.2 **********"
+##
+##print " | | "
+##print "-----"
+##print " | | "
+##print "-----"
+##print " | | "
+##
+##print "********** Exercise 1.3 **********"
+##
+##a = " |"
+##b = "-"
+##print a*2
+##print b*5
+##print a*2
+##print b*5
+##print a*2
+
+print "********** Exercise 1.4 **********"
+print "********* Part II *************"
+
+print "1.", (3*5)/(2+3)
+print "2.",((7+9)**0.5)*2
+print "3.",(4-7)**3
+print "4.",(-19+100)**0.25
+print "5.", 6/4
+
+print "********* Part III *************"
+
+a = (25+19)*(13-7)
+b = 25+19*13-7
+
+print a, b
